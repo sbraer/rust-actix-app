eadfa277f5179163498ee60d0a2992843ac541cb
AZ <sbraer@gmail.com>
2024-12-27 11:24:00 +0100

    removed check error

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-mod errors;
 mod person;
 mod routes;
 
--- src/routes.rs
+++ src/routes.rs
@@ -1,7 +1,6 @@
 use actix_web::{get, HttpResponse};
 use std::ops::Deref;
 
-use crate::errors::AppResponse;
 use crate::person::{Person, RandomGenerator};
 
 #[get("/health")]
@@ -10,7 +9,7 @@ async fn health() -> HttpResponse {
 }
 
 #[get("/api/random_persons")]
-async fn random_persons() -> AppResponse {
+async fn random_persons() -> HttpResponse {
     let items: Vec<Person> = (0..100).map(|_| Person::random()).collect();
-    Ok(HttpResponse::Ok().json(items.deref()))
+    HttpResponse::Ok().json(items.deref())
 }
